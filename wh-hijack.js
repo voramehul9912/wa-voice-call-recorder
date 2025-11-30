3e5ca35c76d50ecb5f22accda44d1d5bfcff54b2
voramehul9912 <129065912+voramehul9912@users.noreply.github.com>
2025-11-30 16:29:15 +0530

    Add files via upload

---
+++ wh-hijack.js
@@ -0,0 +1,430 @@
+var g_data_location = "/data/data/com.whatsapp/cache/voip/"
+var g_filename = "";
+let g_fos_obj = null;
+function toHex(byteArray) {
+    return Array.prototype.map.call(
+        new Uint8Array(byteArray),
+        x => ('00' + x.toString(16)).slice(-2)
+    ).join(' ');
+}
+
+function getIID(name) {
+  const iid_ptr = Module.findExportByName("libOpenSLES.so", name);
+  if (!iid_ptr) {
+      console.log("[!] IID not found:", name);
+      return null;
+  }
+  return iid_ptr.readPointer();
+}
+
+const IID_BUFFERQUEUE = getIID("SL_IID_BUFFERQUEUE");
+// console.log("IID_BUFFERQUEUE : " + IID_BUFFERQUEUE + "\n" + toHex(IID_BUFFERQUEUE.readByteArray(16)));
+const IID_ANDROIDSIMPLEBUFFERQUEUE = getIID("SL_IID_ANDROIDSIMPLEBUFFERQUEUE");
+// console.log("IID_ANDROIDSIMPLEBUFFERQUEUE : " + IID_ANDROIDSIMPLEBUFFERQUEUE + "\n" + toHex(IID_ANDROIDSIMPLEBUFFERQUEUE.readByteArray(16)));
+const IID_RECORD = getIID("SL_IID_RECORD");
+// console.log("IID_RECORD : " + IID_RECORD + "\n" + toHex(IID_RECORD.readByteArray(16)));
+const IID_PLAY = getIID("SL_IID_PLAY");
+// console.log("IID_PLAY : " + IID_PLAY + "\n" + toHex(IID_PLAY.readByteArray(16)));
+const IID_ENGINE = getIID("SL_IID_ENGINE");
+// console.log("IID_ENGINE : " + IID_ENGINE + "\n" + toHex(IID_ENGINE.readByteArray(16)));
+
+function dump_vtable(vt) {
+    for (let i = 0; i < 25; i++) {
+        let fn = vt.add(i * Process.pointerSize).readPointer();
+        console.log("vtable[" + i + "] = " + fn);
+    }
+}
+
+function generate_random_string(len = 16) {
+  const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let out = "";
+  for (let i = 0; i < len; i++) {
+      out += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return out;
+}
+
+function timestamp() {
+  return new Date().toISOString(); // e.g. "2025-01-25T14:22:05.123Z"
+}
+
+function open_pcm_file(filepath) {
+  if (g_fos_obj == null) {
+      var file_class = Java.use("java.io.File");
+      var fos_class = Java.use("java.io.FileOutputStream");
+      var file_obj = file_class.$new(filepath);
+      g_fos_obj = fos_class.$new(file_obj, /* append */ true);
+      console.log("[+] PCM output file opened:", filepath);
+  }
+}
+
+function close_pcm_file(filepath) {
+  if (g_fos_obj != null) {
+    Java.perform(function() {
+        g_fos_obj.close();
+    });
+    console.log("[+] PCM output file closed");
+  }
+}
+
+function buffer_to_array(ab) {
+    const u8 = new Uint8Array(ab);
+    const out = [];
+    for (let i = 0; i < u8.length; i++) out.push(u8[i]);
+    return out;
+}
+
+function dump_pcm_data(buffer_ptr, buffer_size) {
+  if (g_fos_obj == null || buffer_size <= 0) return;
+  var data = buffer_ptr.readByteArray(buffer_size);
+  Java.perform(function() {
+    if (!g_fos_obj) {
+      return;
+    }
+    var byte_array = Java.array('byte', buffer_to_array(data));
+    g_fos_obj.write(byte_array, 0, buffer_size);
+    g_fos_obj.flush();  
+  });
+}
+
+function iidEquals(a1, b1) {
+  const a = a1.readByteArray(16);
+  if (a == null) return false;
+  const b = b1.readByteArray(16);
+  if (b === null) return false;
+  const a8 = new Uint8Array(a);
+  const b8 = new Uint8Array(b);
+  if (a8.length !== b8.length) return false;
+  for (let i = 0; i < 16; i++) {
+    if (a8[i] !== b8[i]) return false;
+  }
+  return true;
+}
+
+function get_audio_src(audio_src_ptr) {
+  try {
+    const audio_src = audio_src_ptr.readPointer(); // SLDataSource
+    console.log("audio src :", audio_src);
+    const p_locator = audio_src.readPointer();
+    console.log("locator address : ", p_locator);
+    const locator_type = p_locator.readU32();
+    console.log("Locator type : " + locator_type);
+    const p_format = audio_src.add(Process.pointerSize).readPointer();
+    console.log("format address : ", p_format);
+    const format_type     = p_format.readU32();
+    const num_channels    = p_format.add(4).readU32();
+    const samples_per_sec  = p_format.add(8).readU32();
+    const bits_per_sample  = p_format.add(12).readU32();
+    const container_size  = p_format.add(16).readU32();
+    const channel_mask    = p_format.add(20).readU32();
+    const endianness     = p_format.add(24).readU32();
+    console.log("  format type:       " + format_type);
+    console.log("  num channels:      " + num_channels);
+    console.log("  sample rate:       " + samples_per_sec);
+    console.log("  bits/sample:       " + bits_per_sample);
+    console.log("  endian:            " + endianness);
+    console.log("  container_size:    " + container_size);
+    console.log("  channel_mask:      " + channel_mask);
+  } catch (e) {
+    console.error("PCM format read error:", e);
+  }
+
+}
+
+function hook_realize(realize) {
+  Interceptor.attach(realize, {
+    onEnter: function(args) {
+      // console.log("Realize onEnter async : " + args[1]);
+    },
+    onLeave: function(retval) {
+      // console.log("Realize onLeave return value : " + retval);
+    }
+  });
+}
+
+function hook_get_interface(get_interface_ptr) {
+  Interceptor.attach(get_interface_ptr, {
+    onEnter: function(args) {
+      this.iid = args[1];
+      this.p_interface_ptr = args[2];
+      // console.log("GetInterface onEnter iid : " + this.iid + " p_interface_ptr : " + this.p_interface_ptr);
+    },
+    onLeave: function(retval) {
+      // console.log("GetInterface onLeave");
+      // console.log("IID : " + toHex(this.iid.readByteArray(16)));
+      const p_interface = this.p_interface_ptr.readPointer();
+      const vtble = p_interface.readPointer();
+      // console.log("Dumping vtable.");
+      // dump_vtable(vtble);
+      if (iidEquals(this.iid, IID_ENGINE)) {
+        console.log("Found IID_ENGINE : " + toHex(this.iid.readByteArray(16)));
+        const create_audio_player_ptr = vtble.add(2 * Process.pointerSize).readPointer();
+        hook_create_audio_player(create_audio_player_ptr);
+        const create_audio_recorder_ptr = vtble.add(3 * Process.pointerSize).readPointer();
+        hook_create_audio_recorder(create_audio_recorder_ptr);
+      } else if (iidEquals(this.iid, IID_ANDROIDSIMPLEBUFFERQUEUE)) {
+        console.log("Found IID_ANDROIDSIMPLEBUFFERQUEUE : " + toHex(this.iid.readByteArray(16)));
+        const enqueue_ptr = vtble.add(0 * Process.pointerSize).readPointer();
+        // console.log(`[+] RECORD Enqueue @ ${enqueue_ptr}`);
+        hook_enqueue(enqueue_ptr);
+        const clear_ptr = vtble.add(1 * Process.pointerSize).readPointer();
+        hook_clear(clear_ptr);
+      } else if (iidEquals(this.iid, IID_BUFFERQUEUE)) {
+        // console.log("Found IID_BUFFERQUEUE : " + toHex(this.iid.readByteArray(16)));
+        const clear_ptr = vtble.add(1 * Process.pointerSize).readPointer();
+        hook_clear(clear_ptr);
+      }
+    }
+  });
+}
+
+function hook_create_audio_player(create_audio_player_ptr) {
+  Interceptor.attach(create_audio_player_ptr, {
+    onEnter: function(args) {
+      console.log("CreateAudioPlayer onEnter");
+      this.self_obj = args[0];
+      this.p_player_ptr = args[1];
+      this.p_audio_src_ptr = args[2];
+      this.p_audio_snk_ptr = args[3];
+      this.num_interfaces = args[4];
+      this.p_interface_ids_ptr = args[5];
+      this.p_interface_required_ptr = args[6];
+      // console.log("player ptr : " + this.p_player_ptr);
+    },
+    onLeave: function(retval) {
+      console.log("CreateAudioPlayer onLeave");
+      if (retval.toInt32() == 0) {
+        get_audio_src(this.p_audio_src_ptr);
+        const p_player_obj = this.p_player_ptr.readPointer();
+        // console.log("player obj : " + p_player_obj);
+        const vtble = p_player_obj.readPointer();
+        // console.log("vtable : " + vtble);
+        const realize_ptr = vtble.add(0 * Process.pointerSize).readPointer();
+        // console.log("Realize : " + realize_ptr);
+        if (realize_ptr) {
+          hook_realize(realize_ptr);
+        }
+        const resume_ptr = vtble.add(1 * Process.pointerSize).readPointer();
+        // console.log("Resume : " + resume_ptr);
+        const get_state_ptr = vtble.add(2 * Process.pointerSize).readPointer();
+        // console.log("GetState : " + get_state_ptr);
+        const get_interface_ptr = vtble.add(3 * Process.pointerSize).readPointer();
+        // console.log("GetInterface : " + get_interface_ptr);
+        if (get_interface_ptr) {
+          hook_get_interface(get_interface_ptr);
+        }
+        const register_callback_ptr = vtble.add(4 * Process.pointerSize).readPointer();
+        // console.log("RegisterCallback : " + register_callback_ptr);
+      } else {
+        console.log("CreateAudioPlayer failed. ignoring.");
+      }
+    }
+  });
+}
+
+function hook_create_audio_recorder(create_audio_recorder_ptr) {
+  Interceptor.attach(create_audio_recorder_ptr, {
+    onEnter: function(args) {
+      console.log("CreateAudioRecorder onEnter");
+      this.self_obj = args[0];
+      this.p_recorder_ptr = args[1];
+      this.p_audio_src_ptr = args[2];
+      this.p_audio_snk_ptr = args[3];
+      this.num_interfaces = args[4];
+      this.p_interface_ids_ptr = args[5];
+      this.p_interface_required_ptr = args[6];
+    },
+    onLeave: function(retval) {
+      console.log("CreateAudioRecorder onLeave");
+      if (retval.toInt32() == 0) {
+        get_audio_src(this.p_audio_src_ptr);
+        const p_recorder_obj = this.p_recorder_ptr.readPointer();
+        // console.log("recorder obj : " + p_recorder_obj);
+        const vtble = p_recorder_obj.readPointer();
+        // console.log("vtable : " + vtble);
+        const realize_ptr = vtble.add(0 * Process.pointerSize).readPointer();
+        // console.log("Realize : " + realize_ptr);
+        if (realize_ptr) {
+          hook_realize(realize_ptr);
+        }
+        const resume_ptr = vtble.add(1 * Process.pointerSize).readPointer();
+        // console.log("Resume : " + resume_ptr);
+        const get_state_ptr = vtble.add(2 * Process.pointerSize).readPointer();
+        // console.log("GetState : " + get_state_ptr);
+        const get_interface_ptr = vtble.add(3 * Process.pointerSize).readPointer();
+        // console.log("GetInterface : " + get_interface_ptr);
+        if (get_interface_ptr) {
+          hook_get_interface(get_interface_ptr);
+        }
+        const register_callback_ptr = vtble.add(4 * Process.pointerSize).readPointer();
+        // console.log("RegisterCallback : " + register_callback_ptr);
+      } else {
+        console.log("CreateAudioPlayer failed. ignoring.");
+      }
+    }
+  });
+}
+
+function hook_enqueue(enqueue_ptr) {
+  Interceptor.attach(enqueue_ptr, {
+    onEnter: function(args){
+      // console.log("Enqueue onEnter");
+      const buffer_ptr = args[1];
+      const buffer_size   = args[2].toInt32();
+      // console.log(`[RECORD] PCM size=${buffer_size} buf=${buffer_ptr}`);
+      if (buffer_ptr && !buffer_ptr.isNull() && buffer_size > 0) {
+        // console.log("[RECORD] dumping PCM data");
+        dump_pcm_data(buffer_ptr, buffer_size);
+      }
+    }, 
+    onLeave: function(retval) {
+      // console.log("Enqueue onLeave");
+    }
+  });
+}
+
+function hook_clear(clear_ptr) {
+  Interceptor.attach(clear_ptr, {
+    onEnter: function(args){
+      // console.log("Clear onEnter");
+      const buffer_ptr = args[1];
+      const buffer_size   = args[2].toInt32();
+      // console.log(`[PLAYBACK] PCM size=${buffer_size} buf=${buffer_ptr}`);
+      if (buffer_ptr && !buffer_ptr.isNull() && buffer_size > 0) {
+        // console.log("[PLAYBACK] dumping PCM data");
+        dump_pcm_data(buffer_ptr, buffer_size);
+      }
+    },
+    onLeave: function(retval) {
+      // console.log("Clear onLeave");
+    }
+  });
+}
+
+function install_hooks_for_sl_create_engine() {
+  console.log("Hooking slCreateEngine");
+  var lib_opensles_so = "libOpenSLES.so";
+  var success = false;
+  var exp_syms = Module.enumerateExports(lib_opensles_so);
+  exp_syms.forEach(sym => {
+    if (sym.name.includes("slCreateEngine")) {
+      console.log("Found slCreateEngine");
+      success = true;
+      Interceptor.attach(sym.address, {
+        onEnter: function(args) {
+          // console.log("slCreateEngine onEnter " + " p_engine_ptr : " + args[0] + " num options : " + args[1] + " p_engine_options_ptr : " + args[2] + " num interfaces : " + args[3] + " p_interface_ids ptr : " + args[4] + " p_interface_required : " + args[5]);
+          this.p_engine_ptr = args[0];
+          success = true;
+        },
+        onLeave: function(retval) {
+          // console.log("slCreateEngine onLeave. return value : " + retval);
+          if (!this.p_engine_ptr) {
+            // console.log("p_engine_ptr is null");
+            return retval;
+          }
+          const p_engine_obj = this.p_engine_ptr.readPointer();
+          // console.log("p_engine object : " + p_engine_obj);
+          if (p_engine_obj.isNull()) {
+            // console.log("p_engine object is NULL");
+            return retval;
+          }
+          const vtble = p_engine_obj.readPointer();
+          // console.log("vtable : " + vtble);
+          const realize_ptr = vtble.add(0 * Process.pointerSize).readPointer();
+          // console.log("Realize : " + realize_ptr);
+          if (realize_ptr) {
+            hook_realize(realize_ptr);
+          }
+          const resume_ptr = vtble.add(1 * Process.pointerSize).readPointer();
+          // console.log("Resume : " + resume_ptr);
+          const get_state_ptr = vtble.add(2 * Process.pointerSize).readPointer();
+          // console.log("GetState : " + get_state_ptr);
+          const get_interface_ptr = vtble.add(3 * Process.pointerSize).readPointer();
+          // console.log("GetInterface : " + get_interface_ptr);
+          if (get_interface_ptr) {
+            hook_get_interface(get_interface_ptr);
+          }
+          const register_callback_ptr = vtble.add(4 * Process.pointerSize).readPointer();
+          // console.log("RegisterCallback : " + register_callback_ptr);
+        }
+      });
+    }
+  });
+  return success;
+}
+
+function install_hooks_for_wa_calling_status() {
+  console.log("Hooking whatsapp calling status");
+  var dlopen_addr = Module.findExportByName(null, "android_dlopen_ext");
+  var lib_path = "";
+  var startcall_success = false, endcall_success = false;
+  if (!dlopen_addr) {
+    console.log("dlopen not found.");
+    return false;
+  }
+  Interceptor.attach(dlopen_addr, {
+    onEnter: function(args) {
+      lib_path = Memory.readCString(args[0]);
+    },
+    onLeave: function(retval) {
+      if (lib_path.includes("libwhatsapp.so")) {
+        var syms = Module.enumerateExports(lib_path);
+        syms.forEach(sym => {
+          if (sym.name.includes("Java_com_whatsapp_calling_voipcalling_Voip_startCall")) {
+            startcall_success = true;
+            Interceptor.attach(sym.address, {
+              onEnter: function(args) {
+                console.log("Hijacked startCall.");
+                var rand_str = generate_random_string();
+                var current_time_stamp = timestamp();
+                g_filename = rand_str + "-" + current_time_stamp;
+                open_pcm_file(g_data_location + g_filename);
+              },
+              onLeave: function(retval2) {}
+            });
+          } else if (sym.name.includes("Java_com_whatsapp_calling_voipcalling_Voip_endCall")) {
+            endcall_success = true;
+            Interceptor.attach(sym.address, {
+              onEnter: function(args) {
+                console.log("Hijacked endCall.");
+              },
+              onLeave: function(retval2) {
+                g_fos_obj.flush();
+                close_pcm_file(g_data_location + g_filename);
+              }
+            });
+          } else if (sym.name.includes("Java_com_whatsapp_calling_voipcalling_Voip_acceptCall")) {
+            Interceptor.attach(sym.address, {
+              onEnter: function(args) {
+                console.log("Hijacked acceptCall.");
+              },
+              onLeave: function(retval2) {}
+            });
+          } else if (sym.name.includes("Java_com_whatsapp_calling_voipcalling_Voip_rejectCall")) {
+            Interceptor.attach(sym.address, {
+              onEnter: function(args) {
+                console.log("Hijacked rejectCall.");
+              },
+              onLeave: function(retval2) {}
+            });
+          }
+        });
+      }
+    }
+  });
+  return startcall_success == true && endcall_success == true;
+}
+
+function install_wa_hooks() {
+  console.log("Installing hooks for WhatsApp.")
+  var ret = install_hooks_for_sl_create_engine();
+  if (ret == false) {
+    console.log("Hooking slCreateEngine failed.");
+  }
+  ret = install_hooks_for_wa_calling_status();
+  if (ret == false) {
+    console.log("Hooking start_call and end_call failed.");
+  }
+}
+
+install_wa_hooks();
